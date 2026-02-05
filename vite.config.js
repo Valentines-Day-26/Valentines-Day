62098cff724190264c51a93a674f3079c16b200b
Maxim Balajan <maxim.balajan@fau.de>
2026-02-05 23:02:07 +0100

    jhg

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Valentines-Day',              // ensure relative paths, e.g. in index.html
+  base: '/Valentines-Day/',              // ensure relative paths, e.g. in index.html
   build: {
       outDir: 'dist',      // gets deployed to github pages
       assetsDir: 'assets', // static assets (images, js, css, etc.)
