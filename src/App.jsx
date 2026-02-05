4e71db78393b3b243caf1533e59dcf5aef0f423e
Maxim Balajan <maxim.balajan@fau.de>
2026-02-05 22:08:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState, useRef } from 'react'
+import reactLogo from './assets/flork-heart.png'
+import dance from './assets/dance.gif'
+import heroImg from './assets/hero.png'
+import './App.css'
+import EvilButton from './evil_button'
+import { BrowserRouter, Link, Routes, Route} from 'react-router';
+
+
+function FirstPage() {
+
+  return (
+    <div id="div1">
+      <img style={{ maxWidth: '100px', top: 0, position: 'relative' }} src={reactLogo} />
+      <h1>Lara, willst du mein Valentin sein?</h1>
+      <div style={{ display: 'flex', paddingLeft: '20%'}} >
+        <Link to="/hype" id="jaa-button" className='jaa' style={{textDecoration: 'none', scale: 1.25}}>
+          Yes
+        </Link>
+        <EvilButton></EvilButton>
+      </div>
+      <div id='lowest_paragraph'>Nein wäre schon mega cringe...</div>
+    </div>
+  )
+}
+
+function ResultWindow() {
+
+  return (
+    <div>
+    <div className='background-middle' style={{padding: "20px 150px"}}>
+      <img style={{ width: '300px'}} src={dance} />
+      <h1>LETS GOOO, bin hyped 🥰</h1>
+    </div>
+    </div>
+  )
+}
+
+function MainWindow() {
+  return (
+    <div id="main-window" className='background-middle'>
+      <FirstPage />
+    </div>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainWindow />} />
+            <Route path="/hype" element={<ResultWindow />} />
+          </Routes>
+        </BrowserRouter>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/evil_button.jsx
@@ -0,0 +1,123 @@
+import { useEffect, useRef, useState, useLayoutEffect} from "react"
+
+const OFFSET = 50
+
+export default function EvilButton() {
+  const buttonRef = useRef(null)
+  const state = useState(0)
+  const windowBox = document.body.getBoundingClientRect()
+
+  useEffect(() => {
+    const evilButton = buttonRef.current
+    const yesButton = document.getElementById("jaa-button")
+    const mainWindow = document.getElementById("main-window")
+  
+    if (!evilButton || !yesButton) return
+  
+    const updatePosition = () => {
+      const yesRect = yesButton.getBoundingClientRect()
+
+      console.log(mainWindow.getBoundingClientRect().width)
+  
+      setButtonPosition(
+        evilButton,
+        yesRect.right + mainWindow.getBoundingClientRect().width * 0.4 - yesRect.width,
+        yesRect.top
+      )
+    }
+
+    updatePosition()
+    window.addEventListener("resize", updatePosition)
+  
+    return () => {
+      window.removeEventListener("resize", updatePosition)
+    }
+  }, [])
+
+  useEffect(() => {
+    const evilButton = buttonRef.current
+    if (!evilButton) return
+
+    const handleMouseMove = (e) => {
+      const x = e.pageX
+      const y = e.pageY
+
+
+      const buttonBox = evilButton.getBoundingClientRect()
+
+      const horizontalDistanceFrom = distanceFromCenter(
+        buttonBox.x,
+        x,
+        buttonBox.width
+      )   
+      const verticalDistanceFrom = distanceFromCenter(
+        buttonBox.y,
+        y,
+        buttonBox.height
+      )
+
+      const horizontalOffset = buttonBox.width / 2 + OFFSET
+      const verticalOffset = buttonBox.height / 2 + OFFSET
+
+      if (
+        Math.abs(horizontalDistanceFrom) <= horizontalOffset &&
+        Math.abs(verticalDistanceFrom) <= verticalOffset
+      ) {
+        setButtonPosition(
+          evilButton,
+          buttonBox.x + (horizontalOffset / horizontalDistanceFrom) * 20,
+          buttonBox.y + (verticalOffset / verticalDistanceFrom) * 20
+        )
+      }
+    }
+
+    document.addEventListener("mousemove", handleMouseMove)
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove)
+    }
+  }, [])
+
+  const handleClick = () => {
+    alert("Wow, ich seh schon...")
+    window.close()
+  }
+
+  return (
+    <button className="nee"
+      ref={buttonRef}
+      onClick={handleClick}
+      style={{
+        position: "absolute",
+        top: '40%'
+      }}
+    >
+      Nee
+    </button>
+  )
+}
+
+function setButtonPosition(button, left, top) {
+  const windowBox = document.body.getBoundingClientRect()
+  const buttonBox = button.getBoundingClientRect()
+
+  if (distanceFromCenter(left, windowBox.left, buttonBox.width) < 0) {
+    left = windowBox.right - buttonBox.width - OFFSET
+  }
+  if (distanceFromCenter(left, windowBox.right, buttonBox.width) > 0) {
+    left = windowBox.left + OFFSET
+  }
+  if (distanceFromCenter(top, windowBox.top, buttonBox.height) < 0) {
+    top = windowBox.bottom - buttonBox.height - OFFSET
+  }
+  if (distanceFromCenter(top, windowBox.bottom, buttonBox.height) > 0) {
+    top = windowBox.top + OFFSET
+  }
+
+  button.style.left = `${left}px`
+  button.style.top = `${top}px`
+}
+
+function distanceFromCenter(boxPosition, mousePosition, boxSize) {
+  return boxPosition - mousePosition + boxSize / 2
+}
